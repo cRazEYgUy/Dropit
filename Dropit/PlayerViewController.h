eed15b67bdea93554361eb6fcd0e5c41bc43095f
winsandymyint <winsandymyint@amdon.com>
2015-06-12 18:33:22 +0630

    Create New File named PlayerViewController, Set Table View Cell's Style: Subtitle and Identifier: PlayerCell in Attribute Inspector

---
+++ Dropit/PlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayerViewController.h
+//  Dropit
+//
+//  Created by vampy on 12/6/15.
+//  Copyright (c) 2015 Xenova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerViewController : UITableViewController
+
+@end
